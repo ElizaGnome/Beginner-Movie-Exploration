c529cc15848a5a403d9c761b0a6d767cb8ec886e
ElizaGnome <56051077+ElizaGnome@users.noreply.github.com>
2020-05-03 22:45:29 +0100

    Update Season_1_Op.py

--- Season_1_Op.py
+++ Season_1_Op.py
@@ -2,17 +2,19 @@ import requests
 import bs4 as bs
 
 
-
+#Create a place to store our collected information:
 imdbRating = []
 title = []
+
 #what is the general opinion of buffy season one.
 urlPath = "https://www.imdb.com/list/ls054779807/"
 seasonOne = requests.get (urlPath)
-#print to see if i can access the data.
 
+#print to see if i can access the data.
+print(seasonOne)
 data = bs.BeautifulSoup(seasonOne.content, "html.parser")
 
-#we want the rating and name of episode. Its listed as class: list-description for the rating and
+#we want the rating and name of episode. Start with rating, as it appears to be easiest location when inspecting the page.
 
 for line in data.findAll("div",  {"class" : "list-description"}):
     rating = line.text
@@ -33,3 +35,9 @@ titleScore = []
 for x, y in zip(title, imdbRating[2:13]):
     text = (x, y)
     titleScore.append(text)
+#write to csv
+
+with open("mycsv.csv", "W", newline ="") as f:
+    writer = csv.writer(f, delimiter =",")
+    writer.writerows(titleScore)
+
